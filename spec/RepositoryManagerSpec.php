74b1613123c9e3ea792c606d178345b503b2ef19
Carlos Ortega Huetos <carlos@rechannel.co.uk>
2016-03-07 18:54:57 +0000

    Add an object to manage the low-level repositories

---
+++ spec/RepositoryManagerSpec.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace spec\carlosV2\DumbsmartRepositories;
+
+use carlosV2\DumbsmartRepositories\Exception\RepositoryNotFoundException;
+use Everzet\PersistedObjects\Repository;
+use PhpSpec\ObjectBehavior;
+
+class RepositoryManagerSpec extends ObjectBehavior
+{
+    function it_provides_the_repository_assigned_to_an_object(Repository $repository)
+    {
+        $this->addRepository(\stdClass::class, $repository);
+        $this->getRepositoryForObject(new \stdClass())->shouldReturn($repository);
+    }
+
+    function it_throws_an_exception_if_the_object_has_no_repository_assigned()
+    {
+        $this->shouldThrow(RepositoryNotFoundException::class)->duringGetRepositoryForObject(new \stdClass());
+    }
+
+    function it_provides_the_repository_assigned_to_a_classname(Repository $repository)
+    {
+        $this->addRepository(\stdClass::class, $repository);
+        $this->getRepositoryForClassName(\stdClass::class)->shouldReturn($repository);
+    }
+
+    function it_throws_an_exception_if_the_classname_has_no_repository_assigned()
+    {
+        $this->shouldThrow(RepositoryNotFoundException::class)->duringGetRepositoryForClassName(\stdClass::class);
+    }
+}
---
+++ src/RepositoryManager.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace carlosV2\DumbsmartRepositories;
+
+use carlosV2\DumbsmartRepositories\Exception\RepositoryNotFoundException;
+use Everzet\PersistedObjects\Repository;
+
+class RepositoryManager
+{
+    /**
+     * @var Repository[]
+     */
+    private $repositories;
+
+    public function __construct()
+    {
+        $this->repositories = [];
+    }
+
+    /**
+     * @param string     $className
+     * @param Repository $repository
+     */
+    public function addRepository($className, Repository $repository)
+    {
+        $this->repositories[$className] = $repository;
+    }
+
+    /**
+     * @param object $object
+     *
+     * @return Repository
+     *
+     * @throws RepositoryNotFoundException
+     */
+    public function getRepositoryForObject($object)
+    {
+        return $this->getRepositoryForClassName(get_class($object));
+    }
+
+    /**
+     * @param string $className
+     *
+     * @return Repository
+     *
+     * @throws RepositoryNotFoundException
+     */
+    public function getRepositoryForClassName($className)
+    {
+        if (array_key_exists($className, $this->repositories)) {
+            return $this->repositories[$className];
+        }
+
+        throw new RepositoryNotFoundException();
+    }
+}
